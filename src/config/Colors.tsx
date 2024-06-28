e9f43aae2919b91ed21a4b57f52e850619dc838e
Azim <azim@obssolutions.com.sg>
2024-06-28 22:12:51 +0700

    add configuration svg file

---
+++ src/config/Colors.tsx
@@ -0,0 +1,22 @@
+export const Color = {
+  primary: {
+    dark: '#1E1E1E',
+  },
+  secondary: {
+    dark: '#262626',
+  },
+  warning: {
+    light: '#EF9533',
+  },
+  border: {
+    dark: '#323232',
+  },
+  text: {
+    primary: '#AA5AFA',
+    secondary: '#5D5C5C',
+  },
+  gradient: {
+    parple1: '#A060FA',
+    parple2: '#C800CC',
+  }
+};
---
+++ src/config/Svg.tsx
@@ -0,0 +1,17 @@
+export { default as GroupIcon } from '../assets/image/menu_icon/group.svg'
+export { default as HalfmoonIcon } from '../assets/image/menu_icon/half-moon.svg'
+export { default as QRCodeIcon } from '../assets/image/menu_icon/qr-code.svg'
+export { default as StartIcon } from '../assets/image/menu_icon/start.svg'
+export { default as UserIcon } from '../assets/image/menu_icon/user.svg'
+
+
+
+export { default as Search } from '../assets/image/icons/search.svg'
+export { default as ArrowLeft } from '../assets/image/icons/arrow-left.svg'
+
+
+export { default as Disco} from '../assets/image//best_place/disco-light 1.svg'
+export { default as Karaoke} from '../assets/image//best_place/karaoke 1.svg'
+export { default as Bottle} from '../assets/image//best_place/beer 1.svg'
+export { default as Bar} from '../assets/image//best_place/wine-bottle 1.svg'
+
